9737b8c840582ec6f6507875cb8b17db01879111
Kamogelo Mmopane <mmopanemoroka@gmail.com>
2024-10-01 09:48:21 +0200

    condition for cases

--- TDD/password_validator.py
+++ TDD/password_validator.py
@@ -1,8 +1,18 @@
+import re
+
 def is_password_secure(password):
     # Must be at least 8 characters long
     password_length = len(password)
     
     if password_length < 8:
         return False
+    
+     # Must contain both uppercase and lowercase letters
+    upper_case = r'[A-Z]'
+    lower_case = r'[a-z]'
+
+    if not re.search(upper_case, password) or not re.search(lower_case, password):
+        return False
+
     
     return True
