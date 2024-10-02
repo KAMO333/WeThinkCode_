d95bdff6ec29e8acb73746a4d08bb22ae65713e8
Kamogelo Mmopane <mmopanemoroka@gmail.com>
2024-10-02 23:55:54 +0200

    test two digits

--- Week 3/TDD/roman_numeral/test_number_representations.py
+++ Week 3/TDD/roman_numeral/test_number_representations.py
@@ -4,3 +4,10 @@ from number_representations import to_roman_numeral
 class TestToRomanNumeral(unittest.TestCase):
     def test_zero(self):
         self.assertIsNone(to_roman_numeral(0)) 
+
+    def test_single_digit(self):
+        self.assertEqual(to_roman_numeral(1), "I")
+        self.assertEqual(to_roman_numeral(3), "III")
+        self.assertEqual(to_roman_numeral(4), "IV")
+        self.assertEqual(to_roman_numeral(9), "IX") 
+
