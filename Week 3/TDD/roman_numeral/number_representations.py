62f904005d6301bd39ec5420dd37f8cf12d858d0
Kamogelo Mmopane <mmopanemoroka@gmail.com>
2024-10-03 00:03:08 +0200

    program

--- Week 3/TDD/roman_numeral/number_representations.py
+++ Week 3/TDD/roman_numeral/number_representations.py
@@ -1,2 +1,17 @@
 def to_roman_numeral(number):
-    pass
+    roman_numbers = {
+        1000: 'M', 900: 'CM', 500: 'D', 400: 'CD',
+        100: 'C', 90: 'XC', 50: 'L', 40: 'XL',
+        10: 'X', 9: 'IX', 5: 'V', 4: 'IV',
+        1: 'I'
+    }
+    
+     
+    result = ''
+
+    for actual_num, letter in roman_numbers.items():
+        while number >= actual_num:
+            result += letter
+            number -= actual_num 
+
+    return result
