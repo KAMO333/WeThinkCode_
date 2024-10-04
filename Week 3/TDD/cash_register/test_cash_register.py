64646f79d54aa3c09e0f3039f63e539b4aab2e5a
Kamogelo Mmopane <mmopanemoroka@gmail.com>
2024-10-04 16:14:47 +0200

    medium change tests

--- Week 3/TDD/cash_register/test_cash_register.py
+++ Week 3/TDD/cash_register/test_cash_register.py
@@ -8,4 +8,10 @@ class TestChange(unittest.TestCase):
         self.assertEqual(change(50, 100), {'50c': 1})
         self.assertEqual(change(25, 100), {'50c': 1, '20c': 1, '5c': 1})
         self.assertEqual(change(200, 500), {'R2': 1, 'R1': 1})
+    
+    def test_medium_change(self):
+        self.assertEqual(change(2300, 5000), {'R20': 1, 'R5': 1, 'R2': 1})
+        self.assertEqual(change(1000, 2500), {'R10': 1, 'R5': 1})
+        self.assertEqual(change(2550, 6000), {'R20': 1, 'R10': 1, 'R2': 2, '50c': 1})
+        self.assertEqual(change(600, 1200), {'R5':1, 'R1': 1})
 
