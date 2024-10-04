843df0aa8ddaf85d2db7bb33c784fdc67e94e29c
Kamogelo Mmopane <mmopanemoroka@gmail.com>
2024-10-04 12:01:34 +0200

    make change

--- Week 3/TDD/cash_register/cash_register.py
+++ Week 3/TDD/cash_register/cash_register.py
@@ -1,2 +1,6 @@
 def change(amount_due, amount_paid):
-    pass
+    cash = ['R200', 'R100', 'R50', 'R20', 'R10', 'R5', 'R2', 'R1', '50c', '20c', '10c', '5c']
+
+    if amount_paid < amount_due: return 'Insufficient fund' 
+
+    change_is = amount_due - amount_paid
