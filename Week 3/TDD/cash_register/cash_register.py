0b97d69c7a49dc7281e1189db479410824560e12
Kamogelo Mmopane <mmopanemoroka@gmail.com>
2024-10-04 16:13:49 +0200

    small change tests

--- Week 3/TDD/cash_register/cash_register.py
+++ Week 3/TDD/cash_register/cash_register.py
@@ -1,7 +1,7 @@
 def change(amount_due, amount_paid):
     cash = ['R200', 'R100', 'R50', 'R20', 'R10', 'R5', 'R2', 'R1', '50c', '20c', '10c', '5c']
 
-    if amount_paid < amount_due: return 'Insufficient fund' 
+    if amount_due > amount_paid: return 'Insufficient funds'
 
     change_is = amount_due - amount_paid
 
--- Week 3/TDD/cash_register/test_cash_register.py
+++ Week 3/TDD/cash_register/test_cash_register.py
@@ -1,2 +1,11 @@
 import unittest
 from cash_register import change
+
+class TestChange(unittest.TestCase):
+    def test_small_change(self):
+        self.assertEqual(change(250, 1000), {'R5': 1, 'R2': 1, '50c': 1})
+        self.assertEqual(change(300, 500), {'R2': 1})
+        self.assertEqual(change(50, 100), {'50c': 1})
+        self.assertEqual(change(25, 100), {'50c': 1, '20c': 1, '5c': 1})
+        self.assertEqual(change(200, 500), {'R2': 1, 'R1': 1})
+
